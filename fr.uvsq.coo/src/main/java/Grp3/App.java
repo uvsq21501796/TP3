6fc1227eb858ed9bedd4f47fbcfae81fba663fa4
uvsq21501796 <elfachtali.ayman@gmail.com>
2015-11-19 02:08:46 +0100

    test1

--- fr.uvsq.coo/src/main/java/Grp3/App.java
+++ fr.uvsq.coo/src/main/java/Grp3/App.java
@@ -1,8 +1,13 @@
 package Grp3;
 
-public class MyUnit {
-
-    public String concatenate(String one, String two){
-        return one + two;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
     }
 }
--- fr.uvsq.coo/src/test/java/Grp3/AppTest.java
+++ fr.uvsq.coo/src/test/java/Grp3/AppTest.java
@@ -3,18 +3,36 @@ package Grp3;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
-import org.junit.Test;
-import static org.junit.Assert.*;
 
-public class MyUnitTest {
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
 
-    @Test
-    public void testConcatenate() {
-        MyUnit myUnit = new MyUnit();
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
 
-        String result = myUnit.concatenate("one", "two");
-
-        assertEquals("onetwo", result);
-
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
     }
 }
