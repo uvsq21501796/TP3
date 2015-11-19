1adb92297db774f3184f911e3f6f14b6467c0f49
Aiman <elfachtali.ayman@gmail.com>
2015-11-19 12:52:23 +0100

    un essai de changement

--- fr.uvsq.coo/src/main/java/fr/uvsq/coo/ex3_8/personnel1.java
+++ fr.uvsq.coo/src/main/java/fr/uvsq/coo/ex3_8/personnel1.java
@@ -1,6 +1,51 @@
 package fr.uvsq.coo.ex3_8;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
+import java.util.StringJoiner;
+
 public class personnel1 {
+	
+	private String nom;
+	private String prenom;
+	private Date date;
+	private ArrayList<> Tel;
+ 	public fonction fonct;
+	
+	public static class Builder
+	{
+		private String nom = "EL FACHTALI";
+		private String prenom = "Aiman";
+		private Date date;
+ 		private String fonction;
+		public fonction fonct;
+		
+		public Builder Nom(String N) {
+			nom = N;
+			return this;
+		}
+		public Builder Prenom(String P) {
+			prenom = P;
+			return this;	
+		}
+		public Builder(Date date) {
+			super();
+			this.date = date;
+		}
+		public Builder(fr.uvsq.coo.ex3_8.fonction fonct) {
+			super();
+			this.fonct = fonct;
+		}
+		
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 
--- fr.uvsq.coo/src/main/java/fr/uvsq/coo/ex3_8/test.java
+++
@@ -1,5 +0,0 @@
-package fr.uvsq.coo.ex3_8;
-
-public class test {
-
-}
